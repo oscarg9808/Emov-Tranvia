ce2cc23c5948d2159920d7711ef9e847cc83ede8
oscarg9808 <oscarg9808@hotmail.com>
2020-05-02 22:42:47 -0500

    Sonido ambiente

---
+++ Assets/Script/Tranvias/MoverTD.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverTD : MonoBehaviour
+{
+    private float movementSpeed = 1;
+    Vector3 TargetPosition;
+    private float targetTime = 5.0f;
+    private bool bandera = true;
+    private float timer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bandera)
+        {
+            transform.Translate(-Vector3.left * Time.deltaTime * movementSpeed);
+
+            if (transform.position.x > 4.00f)
+            {
+                TargetPosition = transform.position;
+                TargetPosition.x -= 18f;
+                transform.position = TargetPosition;
+            }
+        }
+
+        /*if (transform.position.x < 2.6f && transform.position.x > 1.6f)
+        {
+            //correcto
+            bandera = false;
+            timer += Time.deltaTime;
+            Debug.Log(timer);
+            if (timer > 5.0f)
+            {
+                bandera = true;
+                timer = 0.0f;
+            }
+
+        }
+        else if (transform.position.x > 250.0f)
+        {
+            TargetPosition = transform.position;
+            TargetPosition.x -= 800f;
+            transform.position = TargetPosition;
+        }*/
+
+    }
+}
---
+++ Assets/Script/Tranvias/MoverTI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverTI : MonoBehaviour {
+
+    private float movementSpeed = 1;
+    Vector3 TargetPosition;
+    private float targetTime = 5.0f;
+    private bool bandera = true;
+    private float timer = 0.0f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (bandera)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * movementSpeed);
+
+            if (transform.position.x < -15.92f)
+            {
+                TargetPosition = transform.position;
+                TargetPosition.x += 15f;
+                transform.position = TargetPosition;
+            }
+        }
+    }
+}
